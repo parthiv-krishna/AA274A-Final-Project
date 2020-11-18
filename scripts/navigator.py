2a9f4842ba8968081a488689550ffc2415d67de4
Parthiv Krishna <parthiv-krishna@users.noreply.github.com>
2020-11-17 17:52:33 -0800

    re-enable inflated map in navigator

--- scripts/navigator.py
+++ scripts/navigator.py
@@ -106,8 +106,8 @@ class Navigator:
 
         self.cfg_srv = Server(NavigatorConfig, self.dyn_cfg_callback)
 
-        #rospy.Subscriber('/map_inflated', OccupancyGrid, self.map_callback)
-        rospy.Subscriber('/map', OccupancyGrid, self.map_callback)
+        rospy.Subscriber('/map_inflated', OccupancyGrid, self.map_callback)
+        #rospy.Subscriber('/map', OccupancyGrid, self.map_callback)
         rospy.Subscriber('/map_metadata', MapMetaData, self.map_md_callback)
         rospy.Subscriber('/cmd_nav', Pose2D, self.cmd_nav_callback)
         rospy.Subscriber('/should_stop', Bool, self.should_stop_callback)
